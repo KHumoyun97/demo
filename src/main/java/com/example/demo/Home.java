c9e6f859098feaf5ae7f52abe7b76919ab5cb852
Khumoyun Sharifjonov <sharifjonovhumoyun9779@gmail.com>
2021-06-16 12:09:49 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/Home.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+   public class Home {
+    @GetMapping("/xafa")
+    public String hello() {
+        return "Humoyun";
+    }
+}
